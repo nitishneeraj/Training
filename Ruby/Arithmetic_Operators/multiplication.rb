f976d477eace83d9db0c4fa8b9c537d417aaa4f5
Nitish <nitishneeraj16@gmail.com>
2021-10-28 14:11:49 +0530

    done with Arithmetic operator in ruby addition,multiplication,mode And Diviide

---
+++ Ruby/Arithmetic_Operators/multiplication.rb
@@ -0,0 +1,17 @@
+=begin 
+Ruby program to multiply two numbers.
+=end
+
+# input the numbers and converting 
+# them into integer 
+puts "Enter first value: "
+num1=gets.chomp.to_i
+puts "Enter second value: "
+num2=gets.chomp.to_i
+
+# finding multiply 
+multi=num1*num2
+
+# printing the result
+puts "The multiply is #{multi}"
+
